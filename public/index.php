808394fa0c7f1a34575cdd85d5984732a32fed9b
levend <lwdlwd95@gmail.com>
2020-04-13 14:26:12 +0200

    g

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../src/routing.php';
+
+$loader = new Twig\Loader\FilesystemLoader(__DIR__.'/../src/View');
+$twig = new Twig\Environment($loader, []);
+$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
+echo $twig->render('index.html.twig', array(
+    'products' => $products,
+));
